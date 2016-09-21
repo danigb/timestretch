2ee483b39ecffaa7a6107b80d22c55b668e07cb6
danigb <danigb@gmail.com>
2016-09-21 15:04:01 +0200

    Add MASTER branch again

--- examples/src/play.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function (ac) {
-  return function play (when, duration, buffer) {
-    var source = ac.createBufferSource()
-    source.buffer = buffer
-    source.connect(ac.destination)
-    source.start(when, 0, duration)
-  }
-}
